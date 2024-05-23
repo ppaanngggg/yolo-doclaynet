22567f735aa11ab812ea495bd189a8a5370d0017
ppaanngggg <hantian.pang@gmail.com>
2024-05-23 21:14:30 +0800

    update result

--- eval.py
+++ eval.py
@@ -2,9 +2,14 @@ import typer
 from ultralytics import YOLO
 
 
-def main(model: str, datasets: str = "./datasets/data.yaml", split: str = "test"):
+def main(
+    model: str,
+    datasets: str = "./datasets/data.yaml",
+    split: str = "test",
+    batch: int = 8,
+):
     model = YOLO(model)
-    metrics = model.val(data=datasets, split=split)
+    metrics = model.val(data=datasets, split=split, batch=batch)
     print(metrics)
 
 
